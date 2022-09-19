793d2e30b5af12cfce433f92547e4deb597a9c4e
Codehouze <chijioke.emma.maxwell@gmail.com>
2022-09-19 19:02:30 +0100

    Scalfoding Application UI

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./Components/MovieList";
+import MovieListHeading from "./Components/MovieListHeading";
+import SearchBox from "./Components/SearchBox";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?i=${searchValue}&apikey=428f6c2c`;
+    const response = await fetch(url);
+
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
+};
+export default App;
